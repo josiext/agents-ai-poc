876b21cc3f315a17b5972176f4b920952b3142fd
José Núñez <>
2025-06-06 11:10:50 -0400

    fix: configure Dialogflow client for regional endpoints

--- config/initializers/dialogflow.rb
+++ config/initializers/dialogflow.rb
@@ -6,7 +6,18 @@ module Dialogflow
   AGENT_ID = ENV.fetch("DIALOG_FLOW_CX_AGENT_ID")
 
   def self.client
-    @client ||= Google::Cloud::Dialogflow::CX::V3::Sessions::Client.new
+    @client ||= begin
+      # Configure the client with the correct regional endpoint
+      if LOCATION != "global"
+        # For regional endpoints, configure the client with the regional API endpoint
+        Google::Cloud::Dialogflow::CX::V3::Sessions::Client.new do |config|
+          config.endpoint = "#{LOCATION}-dialogflow.googleapis.com"
+        end
+      else
+        # For global endpoint, use default configuration
+        Google::Cloud::Dialogflow::CX::V3::Sessions::Client.new
+      end
+    end
   end
 
   def self.session_path(session_id)
